bf4433ab0074dad22a564c5e5ea88e831baa6ce0
ilee3949 <ilee3949@users.noreply.github.com>
2020-02-27 01:18:25 -0600

    Completed Part II (The TIdyverse Question)
    Completed the tidyverse section of the pset. Also produced and exported two .png files of the barplots produced in the exercise.

--- PDS-PS3.R
+++ PDS-PS3.R
@@ -54,15 +54,39 @@ object_size(dyad) #the reorganized-by-dyad data is 12.1 kb
 #Create New objects as shown in the PSet PDF
 library(fivethirtyeight)
 library(tidyverse)
-polls <- read_csv('https://jmontgomery.github.io/PDS/Datasets/president
-_primary_polls_feb2020.csv')
+polls <- read_csv('https://jmontgomery.github.io/PDS/Datasets/president_primary_polls_feb2020.csv')
 Endorsements <- endorsements_2020
-
-
-
-
-
-
+#Changed the Endorsements variable name endorsee to candidate_name
+Endorsements <- rename(Endorsements, candidate_name=endorsee)
+#Changed the Endorsements dataframe into a tibble object
+as_tibble(Endorsements)
+#Filter to top candidates and subset to the the five variables
+polls = polls %>%
+  filter(candidate_name %in%c("Amy Klobuchar","Bernard Sanders","Elizabeth Warren","Joseph R. Biden Jr.","Michael Bloomberg","Pete Buttigieg")) %>%
+  select(candidate_name,sample_size,start_date,party,pct)
+#Fix name discrepancy
+polls$candidate_name = recode(polls$candidate_name, "Bernard Sanders" = "Bernie Sanders")
+polls$candidate_name = recode(polls$candidate_name, "Joseph R. Biden Jr." = "Joe Biden")
+#Combine two datasets by candidate name using dplyr
+newdata <- inner_join(Endorsements,polls,by='candidate_name')
+#Create variable for num endorsements for each candidate
+end = Endorsements %>% count(candidate_name) %>% filter(candidate_name %in% polls$candidate_name)
+#Plot
+p = ggplot(end,aes(x=candidate_name,y=n))+geom_bar(stat="identity")
+p
+#Apply Dark Theme
+p=p+theme_dark()
+#Save this version of the plot (Saved as RPlot_Q3_Version1.png)
+#Make the graph prettier by adding labels and other cool features
+q= ggplot(end,aes(x=candidate_name,y=n))+geom_bar(stat="identity")+
+  labs(title = "# of Endorsements by Candidate") +
+  labs() +
+  xlab("Candidate Name") +
+  ylab("# of Endorsements") +
+  theme_minimal() 
+q=q+theme_dark()
+q
+#Save this prettier version too (Saves as RPlot_Q3_Version2.png)
 
 
 
